f27ae73423a9ab4c95a6daa35262e8dcb4d50645
m.m <meeetju@gmail.com>
2025-11-09 16:50:07 +0100

    adhoc: Add subscribe method
    Add subsrcibe method and deserialize url encoded parameters

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
-use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, dev::Server, web};
+use actix_web::{App, HttpResponse, HttpServer, Responder, dev::Server, web};
 use std::net::TcpListener;
+use serde::Deserialize;
 
 pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     let server = HttpServer::new(|| {
         App::new()
-            .route("/", web::get().to(greet))
             .route("/health_check", web::get().to(health_check))
-            .route("/{name}", web::get().to(greet))
+            .route("/subscriptions", web::post().to(subscribe))
     })
     .listen(listener)?
     .run();
@@ -14,13 +14,17 @@ pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
     Ok(server)
 }
 
-// curl http://127.0.0.1:8000/lol
-async fn greet(req: HttpRequest) -> impl Responder {
-    let name = req.match_info().get("name").unwrap_or("World");
-    format!("Hello {}!", &name)
-}
-
 //curl -v http://127.0.0.1:8000/health_check
 async fn health_check() -> impl Responder {
-    HttpResponse::Ok()
+    HttpResponse::Ok().finish()
+}
+
+#[derive(serde::Deserialize)]
+pub struct FormData {
+    email: String,
+    name: String,
+}
+
+async fn subscribe(_form: web::Form<FormData>) -> impl Responder {
+    HttpResponse::Ok().finish()
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,6 +1,19 @@
 use std::net::TcpListener;
 use zero::run;
 
+// Launch our application in the background ~somehow~
+async fn spawn_app() -> Result<String, std::io::Error> {
+    let listener = TcpListener::bind("127.0.0.1:0")?;
+    let port = listener.local_addr().unwrap().port();
+
+    let server = run(listener)?;
+    let _ = tokio::spawn(server);
+
+    let address = format!("http://127.0.0.1:{}", port);
+    println!("Server is running on {}", address);
+    Ok(address)
+}
+
 #[tokio::test]
 async fn health_check_works() {
     let address = spawn_app().await.expect("Failed to spawn our app.");
@@ -16,15 +29,42 @@ async fn health_check_works() {
     assert!(response.status().is_success());
     assert_eq!(Some(0), response.content_length());
 }
-// Launch our application in the background ~somehow~
-async fn spawn_app() -> Result<String, std::io::Error> {
-    let listener = TcpListener::bind("127.0.0.1:0")?;
-    let port = listener.local_addr().unwrap().port();
 
-    let server = run(listener)?;
-    let _ = tokio::spawn(server);
+#[tokio::test]
+async fn subscribe_returns_a_200_for_valid_form_data() {
+    let app_address = spawn_app().await.expect("Failed to spawn our app.");
 
-    let address = format!("http://127.0.0.1:{}", port);
-    println!("Server is running on {}", address);
-    Ok(address)
+    let client = reqwest::Client::new();
+
+    let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
+    let response = client.post(&format!("{}/subscriptions", app_address))
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(body)
+        .send()
+        .await.expect("Failed to execute request.");
+
+    assert_eq!(response.status().as_u16(), 200);
+}
+
+#[tokio::test]
+async fn subscribe_returns_a_400_when_data_is_missing() {
+    let app_address = spawn_app().await.expect("Failed to spawn our app.");
+
+    let client = reqwest::Client::new();
+
+    let test_cases = vec![
+        ("name=le%20guin", "missing the email"),
+        ("email=ursula_le_guin%40gmail.com", "missing the name"),
+        ("", "missing both name and email"),
+    ];
+
+    for (invalid_body, error_message) in test_cases {
+        let response = client.post(&format!("{}/subscriptions", app_address))
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .body(invalid_body)
+            .send()
+            .await.expect("Failed to execute request.");
+
+        assert_eq!(response.status().as_u16(), 400, "The API did not fail with 400 Bad Request when the payload was {}", error_message);
+    }
 }
